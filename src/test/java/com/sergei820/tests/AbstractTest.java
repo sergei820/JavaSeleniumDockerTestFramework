e9d6ab9fd042056843caf0a929d7fc5937218b99
sergei820 <se25051990@gmail.com>
2024-01-27 13:10:22 +0400

    Initial commit

---
+++ src/test/java/com/sergei820/tests/AbstractTest.java
@@ -0,0 +1,25 @@
+package com.sergei820.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public abstract class AbstractTest {
+
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void setDriver(){
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void quitDriver(){
+        this.driver.quit();
+    }
+
+}
