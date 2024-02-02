e0a13d23f6c109743c53b8c1d3a90c41f11539cc
sergei820 <se25051990@gmail.com>
2024-02-02 16:26:16 +0400

    Running in Selenium Grid in Docker

--- src/test/java/com/sergei820/tests/AbstractTest.java
+++ src/test/java/com/sergei820/tests/AbstractTest.java
@@ -1,24 +1,49 @@
 package com.sergei820.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class AbstractTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setDriver(){
+    public void setDriver() throws MalformedURLException {
+        if(Boolean.getBoolean("selenium.grid.enabled")) {
+            this.driver = getRemoteDriver();
+        } else {
+            this.driver = getLocalDriver();
+        }
+    }
+
+    private WebDriver getLocalDriver() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
+        return new ChromeDriver();
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        //http://localhost:4444/wd/hub
+        Capabilities capabilities;
+        if(System.getProperty("browser").equalsIgnoreCase("chrome")){
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
     }
 
     @AfterTest
-    public void quitDriver(){
+    public void quitDriver() {
         this.driver.quit();
     }
 
