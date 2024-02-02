f3190f370d87461757a8772c0d52bd858cac0d20
sergei820 <se25051990@gmail.com>
2024-02-02 19:07:51 +0400

    Refactoring in progress

--- src/test/java/com/sergei820/tests/AbstractTest.java
+++ src/test/java/com/sergei820/tests/AbstractTest.java
@@ -1,5 +1,7 @@
 package com.sergei820.tests;
 
+import com.sergei820.util.Config;
+import com.sergei820.util.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
@@ -7,23 +9,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public abstract class AbstractTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AbstractTest.class);
+
     protected WebDriver driver;
 
     @BeforeTest
+    @Parameters({"browser"})
     public void setDriver() throws MalformedURLException {
-        if(Boolean.getBoolean("selenium.grid.enabled")) {
-            this.driver = getRemoteDriver();
-        } else {
-            this.driver = getLocalDriver();
+        this.driver = Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)) ? getRemoteDriver() : getLocalDriver();
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        //http://localhost:4444/wd/hub, localhost
+        Capabilities capabilities = new ChromeOptions();
+        if(Constants.FIREFOX.equalsIgnoreCase(Config.get(Constants.BROWSER))) {
+            capabilities = new FirefoxOptions();
         }
+        String urlFormat = Config.get(Constants.GRID_URL_FORMAT);
+        String hubHost = Config.get(Constants.GRID_HUB_HOST);
+        String url = String.format(urlFormat, hubHost);
+        log.info("grid url: {}", url);
+
+        return new RemoteWebDriver(new URL(url), capabilities);
     }
 
     private WebDriver getLocalDriver() {
@@ -31,16 +49,6 @@ public abstract class AbstractTest {
         return new ChromeDriver();
     }
 
-    private WebDriver getRemoteDriver() throws MalformedURLException {
-        //http://localhost:4444/wd/hub
-        Capabilities capabilities;
-        if(System.getProperty("browser").equalsIgnoreCase("chrome")){
-            capabilities = new ChromeOptions();
-        } else {
-            capabilities = new FirefoxOptions();
-        }
-        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-    }
 
     @AfterTest
     public void quitDriver() {
